56ca442f44360a375bcedb649e9424d4dbd71625
drindalipi_dd <drin.dalipi@i2invest.ch>
2025-08-01 01:00:25 +0200

    Use the constant ACCESS_TOKEN_EXPIRE_MINUTES in the timedelta function

--- app/services/auth.py
+++ app/services/auth.py
@@ -23,7 +23,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     """
 
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
