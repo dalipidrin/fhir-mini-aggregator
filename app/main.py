aae2602216130e07a0ff2b4e24b9bc7a0bfcd476
drindalipi_dd <drin.dalipi@i2invest.ch>
2025-08-01 15:26:28 +0200

    Implement a quick CORS fix so React app can access the endpoints

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI
-from app.routes import routes
 import logging
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routes import routes
 
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI(title="FHIR Mini Aggregator")
 
 app.include_router(routes.router)
+
+# Quick development fix to allow requests from any origin so that React app can access the endpoints
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
